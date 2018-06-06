0f1f45b58e0f3ec19f006fe1ff7ef4cbb965cd49
Lorenzo Gaggini <lg@lgaggini.net>
2018-06-06 11:24:02 +0200

    dual external monitor configuration

--- .screenlayout/dp3-left.sh
+++ .screenlayout/dp3-left.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 1920x0 --rotate normal --output VGA1 --off
+xrandr --output VIRTUAL1 --off --output DP3 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 1920x0 --rotate normal --output VGA1 --off
