d8868e88a7e621c8df0ae4abcb71e7aba5b3fa28
Lorenzo Gaggini <lg@lgaggini.net>
2019-07-22 14:06:18 +0200

    autorandr replace custom scripts

--- bin/check_dp3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-xrandr -q | grep DP3 | grep -q " connected"
-echo $?
--- bin/check_dual_monitor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-xrandr -q | grep DP3 | grep -q " connected" && xrandr -q | grep VGA1 | grep -q " connected"
-echo $?
