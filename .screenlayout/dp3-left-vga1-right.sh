1e5c967da816fe0143f2cf13fd63e1d546b6d457
Lorenzo Gaggini <lg@lgaggini.net>
2019-07-23 08:31:35 +0200

    screen layouts are now managed by autorandr

--- .screenlayout/dp3-left-vga1-right.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --mode 1920x1080 --pos 1920x0 --rotate normal
