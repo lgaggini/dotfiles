068802c2d5d153f70e6ae6ec50fcc716efbf7bcf
Lorenzo Gaggini <lg@lgaggini.net>
2024-08-31 08:23:52 +0200

    updates nvim mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -12,8 +12,12 @@ map("n", "B", "<cmd>enew<CR>", { desc = "buffer new" })
 map("n", "gb", function()
   require("nvchad.tabufline").next()
 end, { desc = "buffer goto next" })
+map("n", "gB", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "buffer goto prev" })
 
 -- tabs
 map("n", "T", "<cmd>tabnew<CR>", { desc = "tab new" })
-map("n", "gt", "<cmd>tabnext<CR>", { desc = "tab cycle" })
+map("n", "gt", "<cmd>tabnext<CR>", { desc = "tab next" })
+map("n", "gT", "<cmd>tabprev<CR>", { desc = "tab prev" })
 map("n", "X", "<cmd>tabclose<CR>", { desc = "tab close" })
