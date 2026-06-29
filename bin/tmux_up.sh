66d05cb7e109c163a7f221afe68daadb61640c01
Lorenzo Gaggini <lg@lgaggini.net>
2026-06-29 10:10:40 +0200

    adds days count to tmux_up script

--- bin/tmux_up.sh
+++ bin/tmux_up.sh
@@ -1,1 +1,9 @@
-awk '{print $1}' /proc/uptime | xargs -I{} date -d@{} -u +%H:%M
+awk '
+  {
+    sec = int($1);                     # whole seconds from /proc/uptime
+    days = sec / 86400;                # 86400 s = 1 day
+    hrs  = (sec % 86400) / 3600;       # remaining hours
+    mins = (sec % 3600) / 60;          # remaining minutes
+    printf "%dd%02d:%02d\n", days, hrs, mins
+  }
+' /proc/uptime
